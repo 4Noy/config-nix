4600591ecbe569ff410cd83cef3baf69b332d098
Noy. <>
2025-06-30 20:18:30 +0200

    feat: a

--- modules/utils/python3.nix
+++ modules/utils/python3.nix
@@ -1,17 +1,30 @@
-{ root, config, pkgs, ... }: {
-  home.packages = with pkgs; [
-    python310Full       # full stdlib + pip
-    python310Packages.virtualenv
-    python310Packages.ipython
-  ];
+{ config, pkgs, lib, root, ... }:
 
-  programs.virtualenvwrapper = {
-    enable       = true;
-    home         = "${config.home.homeDirectory}/.virtualenvs";
-  };
+let
+  vh = "${config.home.homeDirectory}/.virtualenvs";
+in {
+  home.packages = with pkgs; [
+    python310Full
+    python310Packages.virtualenv
+    python310Packages.virtualenvwrapper
+    python310Packages.ipython
+    direnv
+  ];
 
   home.file.".config/pip/pip.conf".text = ''
     [global]
     cache-dir = ${config.home.homeDirectory}/.cache/pip
+  '';
+
+  home.file.".envrc".text = ''
+    export WORKON_HOME="${vh}"
+    export PROJECT_HOME="${config.home.homeDirectory}/Devel"
+    export VIRTUALENVWRAPPER_PYTHON="${pkgs.python310Full}/bin/python3"
+    source "${pkgs.python310Packages.virtualenvwrapper}/bin/virtualenvwrapper.sh"
+  '';
+
+  home.file.".profile".match = "PROFILE_INIT";
+  home.file.".profile".text = ''
+    [[ -f "$HOME/.envrc" ]] && eval "$(direnv hook bash)"
   '';
 }
