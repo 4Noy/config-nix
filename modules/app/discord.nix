2711f4b8c2c5d456a4cbab66e8f4a7b9cbad1600
Noy. <>
2025-06-30 18:54:45 +0200

    feat: a

--- modules/app/discord.nix
+++ modules/app/discord.nix
@@ -2,8 +2,7 @@
 
 let
   discordPkg = pkgs.discord.override { withVencord = true; };
-
-  plantImg     = "${root}/files/wallpapers/discord-wallpaper.jpg";
+  plantImg = "${root}/files/wallpapers/discord-wallpaper.jpg";
 in {
   home.packages = [
     discordPkg
@@ -14,13 +13,11 @@ in {
   xdg.configFile."Vencord/themes/tokyo-night-plants.theme.css".text = lib.concatStringsSep "\n" [
     "@import url(\"https://raw.githubusercontent.com/Dyzean/Tokyo-Night/main/themes/tokyo-night.theme.css\");"
     ""
-    "/* overlay a subtle plant wallpaper */"
     "body {"
-    "  background-image: url(\"file://${config.home.homeDirectory}/.config/Vencord/themes/discord-wallpaper.jpg\");"
+    "  background-image: url(\"file://${config.home.homeDirectory}/.config/Vencord/themes/plant-themed.jpg\");"
     "  background-size: cover;"
     "  background-position: center;"
     "  opacity: 0.85;"
     "}"
   ];
-
 }
