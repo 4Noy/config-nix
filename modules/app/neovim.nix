f25fb42556fe18f31d2789c6893560066bd31623
Noy. <>
2025-06-30 20:57:22 +0200

    feat: a

--- modules/app/neovim.nix
+++ modules/app/neovim.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+
+let
+  # raccourci pour plugins vim
+  vimPlugins = pkgs.vimPlugins;
+in
+{
   programs.neovim = {
     enable = true;
     vimAlias = true;
@@ -20,9 +26,8 @@
       black
     ];
 
-    plugins = with pkgs.vimPlugins; [
+    plugins = with vimPlugins; [
       tokyonight-nvim
-      catppuccin
       lualine-nvim
       bufferline-nvim
       telescope-nvim
@@ -53,11 +58,23 @@
       set clipboard=unnamedplus
       set expandtab
       set smartindent
+      filetype plugin indent on
+
+      "
+      set tabstop=4
+      set shiftwidth=4
+      set softtabstop=4
 
       lua << EOF
-        require('catppuccin').setup{ flavour = 'mocha' }
-        vim.cmd.colorscheme 'catppuccin'
-        require('lualine').setup{ options = { theme = 'catppuccin' } }
+        -- Theme
+        vim.cmd.colorscheme("tokyonight")
+
+        -- Setup lualine
+        require('lualine').setup {
+          options = { theme = 'tokyonight' }
+        }
+
+        -- Setup plugins
         require('bufferline').setup{}
         require('gitsigns').setup{}
         require('nvim-tree').setup{}
@@ -65,49 +82,106 @@
         require('editorconfig').setup{}
         require('Comment').setup{}
 
-        require('nvim-treesitter.configs').setup{
-          highlight={ enable=true },
-          indent={ enable=true },
+        -- Treesitter
+        require('nvim-treesitter.configs').setup {
+          highlight = { enable = true },
+          indent = { enable = true },
         }
 
-        local mason = require('mason')
+        -- Mason
+        local mason = require("mason")
         mason.setup()
-        require('mason-lspconfig').setup{
+
+        require("mason-lspconfig").setup {
           ensure_installed = {
-            'pyright', 'clangd', 'lua_ls', 'tsserver',
-            'bashls', 'jsonls', 'yamlls', 'jdtls'
+            "pyright", "clangd", "lua_ls", "tsserver",
+            "bashls", "jsonls", "yamlls", "jdtls"
           }
         }
 
-        local lsp = require('lspconfig')
-        local caps = require('cmp_nvim_lsp').default_capabilities()
-        for _, ls in ipairs(mason.lspconfig.get_installed_servers()) do
-          lsp[ls].setup{ capabilities = caps }
+        local lspconfig = require("lspconfig")
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+        for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
+          lspconfig[server].setup {
+            capabilities = capabilities,
+          }
         end
 
-        require('cmp').setup{
-          snippet={
-            expand=function(args) require('luasnip').lsp_expand(args.body) end
+        -- Setup nvim-cmp
+        local cmp = require('cmp')
+        local luasnip = require('luasnip')
+
+        cmp.setup {
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
           },
-          mapping=require('cmp').mapping.preset.insert{
-            ['<C-Space>'] = require('cmp').mapping.complete(),
-            ['<CR>'] = require('cmp').mapping.confirm{ select=true },
+          mapping = cmp.mapping.preset.insert {
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<CR>'] = cmp.mapping.confirm { select = true },
           },
-          sources={
-            { name='nvim_lsp' },
-            { name='luasnip' },
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'buffer' },
+            { name = 'path' },
           }
         }
 
-        require('null-ls').setup{
-          sources={
-            require('null-ls').builtins.formatting.black,
-            require('null-ls').builtins.formatting.prettier,
-            require('null-ls').builtins.diagnostics.eslint,
-          }
+        -- Setup null-ls
+        local null_ls = require('null-ls')
+        null_ls.setup {
+          sources = {
+            null_ls.builtins.formatting.black,
+            null_ls.builtins.formatting.prettier,
+            null_ls.builtins.diagnostics.eslint,
+          },
+          on_attach = function(client)
+            if client.supports_method("textDocument/formatting") then
+              vim.api.nvim_clear_autocmds({ group = 0, buffer = 0 })
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = 0,
+                callback = function() vim.lsp.buf.format() end
+              })
+            end
+          end,
         }
 
-        require('nvim_comment').setup{}
+        -- Tab
+        vim.api.nvim_create_autocmd({"FileType"}, {
+          pattern = "*",
+          callback = function()
+            local ft = vim.bo.filetype
+            if ft == "make" or ft == "makefile" then
+              vim.bo.expandtab = false
+              vim.bo.tabstop = 4
+              vim.bo.shiftwidth = 4
+              vim.bo.softtabstop = 4
+            else
+              vim.bo.expandtab = true
+            end
+          end
+        })
       EOF
     '';
   };
