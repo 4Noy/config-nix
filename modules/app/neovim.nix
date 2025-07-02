f83011176fae6d4e963b8afbf59ababc9b9a0339
Noy. <>
2025-07-02 11:55:02 +0200

    feat: a

--- modules/app/neovim.nix
+++ modules/app/neovim.nix
@@ -11,6 +11,7 @@ in {
     extraPackages = with pkgs; [
       python3
       python3Packages.pynvim
+      python3Packages.python-lsp-server
       lua-language-server
       clang-tools
       nodePackages.typescript-language-server
@@ -34,8 +35,6 @@ in {
       vim-fugitive
       nvim-treesitter.withAllGrammars
       nvim-lspconfig
-      mason-nvim
-      mason-lspconfig-nvim
       nvim-cmp
       cmp-nvim-lsp
       cmp-buffer
@@ -84,31 +83,28 @@ in {
           indent    = { enable = true },
         })
 
-        -- Mason & LSPconfig
-        local mason       = safe_require("mason")
-        local mlsp        = safe_require("mason-lspconfig")
-        local lspconfig   = safe_require("lspconfig")
+        -- LSP servers via nix-installed binaries
+        local lspconfig = safe_require("lspconfig")
         local cmp_nvim_lsp = safe_require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp and cmp_nvim_lsp.default_capabilities()
 
-        if mason then mason.setup() end
-
-        if mlsp then
-          local wanted = { "clangd", "lua_ls", "tsserver", "bashls", "jsonls", "yamlls", "jdtls" }
-          local available = mlsp.get_available_servers()
-          local to_install = vim.tbl_filter(function(s)
-            return vim.tbl_contains(available, s)
-          end, wanted)
-
-          mlsp.setup { ensure_installed = to_install }
-
-          local caps = cmp_nvim_lsp and cmp_nvim_lsp.default_capabilities()
-          mlsp.setup_handlers {
-            function(server)
-              local cfg = {}
-              if caps then cfg.capabilities = caps end
-              lspconfig[server].setup(cfg)
-            end
-          }
+        if lspconfig then
+          -- Python
+          lspconfig.pylsp.setup { capabilities = capabilities }
+          -- Lua
+          lspconfig.lua_ls.setup { capabilities = capabilities }
+          -- C/C++
+          lspconfig.clangd.setup { capabilities = capabilities }
+          -- Java
+          lspconfig.jdtls.setup { capabilities = capabilities }
+          -- TypeScript/JavaScript
+          lspconfig.tsserver.setup { capabilities = capabilities }
+          -- Bash
+          lspconfig.bashls.setup { capabilities = capabilities }
+          -- JSON
+          lspconfig.jsonls.setup { capabilities = capabilities }
+          -- YAML
+          lspconfig.yamlls.setup { capabilities = capabilities }
         end
 
         -- Completion
@@ -118,16 +114,8 @@ in {
           cmp.setup {
             snippet = { expand = function(args) if luasnip then luasnip.lsp_expand(args.body) end end },
             mapping = cmp.mapping.preset.insert {
-              ["<Tab>"] = cmp.mapping(function(f)
-                if cmp.visible() then cmp.select_next_item()
-                elseif luasnip and luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-                else f() end
-              end, { "i", "s" }),
-              ["<S-Tab>"] = cmp.mapping(function(f)
-                if cmp.visible() then cmp.select_prev_item()
-                elseif luasnip and luasnip.jumpable(-1) then luasnip.jump(-1)
-                else f() end
-              end, { "i", "s" }),
+              ["<Tab>"]  = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+              ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
               ["<C-Space>"] = cmp.mapping.complete(),
               ["<CR>"]     = cmp.mapping.confirm { select = true },
             },
@@ -162,20 +150,20 @@ in {
           callback = function()
             local ft = vim.bo.filetype
             if ft == "make" or ft == "makefile" then
-              vim.bo.expandtab     = false
-              vim.bo.tabstop       = 4
-              vim.bo.shiftwidth    = 4
-              vim.bo.softtabstop   = 4
+              vim.bo.expandtab = false
+              vim.bo.tabstop = 4
+              vim.bo.shiftwidth = 4
+              vim.bo.softtabstop = 4
             elseif ft == "yaml" or ft == "json" or ft == "lua" then
-              vim.bo.expandtab     = true
-              vim.bo.tabstop       = 2
-              vim.bo.shiftwidth    = 2
-              vim.bo.softtabstop   = 2
+              vim.bo.expandtab = true
+              vim.bo.tabstop = 2
+              vim.bo.shiftwidth = 2
+              vim.bo.softtabstop = 2
             else
-              vim.bo.expandtab     = true
-              vim.bo.tabstop       = 4
-              vim.bo.shiftwidth    = 4
-              vim.bo.softtabstop   = 4
+              vim.bo.expandtab = true
+              vim.bo.tabstop = 4
+              vim.bo.shiftwidth = 4
+              vim.bo.softtabstop = 4
             end
           end,
         })
