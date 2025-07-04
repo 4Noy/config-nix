162698f1a754af2f021d0e4e514302924437d770
Noy. <>
2025-07-04 08:37:16 +0200

    feat: a

--- modules/wm/i3.nix
+++ modules/wm/i3.nix
@@ -4,6 +4,6 @@
     i3status
     feh
   ];
-  #xsession.windowManager.i3.enable = true;
   home.file.".config/i3/config".source = "${root}/files/i3/config";
+  "/bin/lock".source = "${root}/files/bin/lock";
 }
